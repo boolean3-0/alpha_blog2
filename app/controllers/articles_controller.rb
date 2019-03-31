70653428c93f5001eb3ce50fd16cbbbf4c14b12d
Liam O'Carroll <boolean3.0@gmail.com>
2019-03-31 15:06:33 -0500

    Added two partials and the destroy action.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -85,6 +85,17 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+
+    def destroy
+        @article = Article.find(params[:id])
+
+        @article.destroy
+
+        flash[:notice] = "Article was successfully deleted."
+
+        redirect_to articles_path
+    end
+
     # Whitelist whatever has been submitted
     private
     def article_params
