f1df7da708a9cbadaa6f1fbd7fbca99aa2362872
Liam O'Carroll <boolean3.0@gmail.com>
2019-04-06 19:02:00 -0500

    Added validation to the User model. Section 6, lecture 111

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
 
+    validates :username, presence: true, 
+                uniqueness: { case_sensitive: false },
+                length: {minimum: 3, maximum: 25 }
+
+    # Note that apparently this isn't the best regex for validating email addresses.
+    # However the best one is very, very long.
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, presence: true,
+                length: { maximum: 105 },
+                uniqueness: { case_senitive: false },
+                format: { with: VALID_EMAIL_REGEX }
 
 end
