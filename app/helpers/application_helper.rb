674eaadcffe88cf9f8c155c83a20ce26063c6d06
Liam O'Carroll <boolean3.0@gmail.com>
2019-05-19 09:54:00 -0500

    Created the show action for users and added gravatar profile images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+# This is where we put helper functions to be used in our views.
+# Not sure if helper functions defined here can be used in other places...
+
+
 module ApplicationHelper
+
+    # https://en.gravatar.com/
+    def gravatar_for(user, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+        # img-circle is a Bootstrap class
+        # Remember: Ruby automatically returns the return value of the last line in a method
+        image_tag(gravatar_url, alt: user.username, class: "img-circle")
+    end
+
 end
