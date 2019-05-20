ef5af7e737b24edcfeb3161fd548304e5e91e9d4
Liam O'Carroll <boolean3.0@gmail.com>
2019-05-20 17:26:03 -0500

    Added restrictions to the UI so that you can't get to pages you shouldn't be able to from the UI. However, you can still get to such pages using a URL. Section 6, lecture 135

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
-    def home
 
+    def home
+        # We only want users who aren't logged in to be able to see the inbound page.
+        redirect_to articles_path if logged_in?
     end
 
     def about
